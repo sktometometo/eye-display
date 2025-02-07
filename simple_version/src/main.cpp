a11424ca214627e15cd5c40fbf34978da7fcfa44
Koki Shinjo <sktometometo@gmail.com>
2025-02-07 10:14:28 +0900

    Update image files and use new design in simple_version. (#25)
    * Remove unused data and add raw gimp data, add simple version data
    
    * [smiple_version/dat] rename files
    
    * [simple_version/data] fix data
    
    * [raw_data] update sample data
    
    * [simple_version/data] fix data
    
    * [raw_data] save as psd instead of xcf
    
    * [simple_version] support new types of image input
    
    * [simple_version main.cpp] fix sample

--- simple_version/src/main.cpp
+++ simple_version/src/main.cpp
@@ -8,13 +8,35 @@
 const int image_width = 139;
 const int image_height = 139;
 
-const char path_image_eyeball[] = "/eyeball.jpg";
-const char path_image_iris_right[] = "/iris_right.jpg";
-const char path_image_surprised_iris_right[] = "/iris_surprised_right.jpg";
-const char path_image_upperlid_right[] = "/upperlid.jpg";
-const char path_image_angry_upperlid_right[] = "/upperlid_leftside_down.jpg";
-const char path_image_sad_upperlid_right[] = "/upperlid_rightside_down.jpg";
-const char path_image_happy_upperlid_right[] = "/upperlid_happy_right.jpg";
+const char path_image_outline[] = "/outline.jpg";
+const char path_image_iris[] = "/iris.jpg";
+const char path_image_pupil[] = "/pupil.jpg";
+const char path_image_reflex[] = "/reflex.jpg";
+const char path_image_upperlid[] = "/upperlid.jpg";
+
+const char path_image_outline_surprised[] = "/outline.jpg";
+const char path_image_iris_surprised[] = "/iris_surprised.jpg";
+const char path_image_pupil_surprised[] = "/pupil_surprised.jpg";
+const char path_image_reflex_surprised[] = "/reflex_surprised.jpg";
+const char path_image_upperlid_surprised[] = "/upperlid.jpg";
+
+const char path_image_outline_angry[] = "/outline.jpg";
+const char path_image_iris_angry[] = "/iris.jpg";
+const char path_image_pupil_angry[] = "/pupil.jpg";
+const char path_image_reflex_angry[] = "/reflex.jpg";
+const char path_image_upperlid_angry[] = "/upperlid_angry.jpg";
+
+const char path_image_outline_sad[] = "/outline.jpg";
+const char path_image_iris_sad[] = "/iris.jpg";
+const char path_image_pupil_sad[] = "/pupil.jpg";
+const char path_image_reflex_sad[] = "/reflex.jpg";
+const char path_image_upperlid_sad[] = "/upperlid_sad.jpg";
+
+const char path_image_outline_happy[] = "/outline.jpg";
+const char path_image_iris_happy[] = "/white.jpg";
+const char path_image_pupil_happy[] = "/white.jpg";
+const char path_image_reflex_happy[] = "/reflex_happy.jpg";
+const char path_image_upperlid_happy[] = "/white.jpg";
 
 static Eye eye;
 
@@ -28,7 +50,8 @@ void setup()
 
   delay(5000);
 
-  eye.init(path_image_eyeball, path_image_iris_right, NULL, NULL, path_image_upperlid_right, image_width, image_height, 1);
+  eye.init(path_image_outline, path_image_iris, path_image_pupil, path_image_reflex,
+          path_image_upperlid, image_width, image_height, 1);
   eye.update_look();
 
   Serial.println("Start.");
@@ -36,77 +59,89 @@ void setup()
 
 void loop()
 {
+  bool success;
+
   // 通常
+  Serial.println("Normal");
+  eye.load_eye_images(path_image_outline, path_image_iris, path_image_pupil, path_image_reflex, path_image_upperlid);
   float look_x, look_y;
-  eye.ready_for_normal_eye(path_image_iris_right, path_image_upperlid_right);
   for (int i=0; i<50; i++) {
       delay(20);
-      look_x = 0.3 * sin(i * 0.1);
-      look_y = 0.3 * cos(i * 0.1) ;
+      look_x = 1.0 * sin(i * 0.1);
+      look_y = 1.0 * cos(i * 0.1) ;
       eye.update_look(look_x, look_y);
-      eye.draw_updated_image();
   }
   look_x = 0;
   look_y = 0;
   eye.update_look(look_x, look_y);
   delay(1000);
 
+  // まばたき
+  Serial.println("Blink");
+  eye.load_eye_images(path_image_outline, path_image_iris, path_image_pupil, path_image_reflex, path_image_upperlid);
   int blink_level = 0;
-  eye.ready_for_normal_eye(path_image_iris_right, path_image_upperlid_right);
   for (int i=0; i<50; i++) {
       delay(20);
       blink_level = i % Eye::max_blink_level;
       eye.blink_eye(look_x, look_y, blink_level);
-      eye.draw_updated_image();
   }
   delay(1000);
 
+  // 驚き
+  Serial.println("Surprised");
+  eye.load_eye_images(path_image_outline_surprised, path_image_iris_surprised,
+          path_image_pupil_surprised, path_image_reflex_surprised, path_image_upperlid_surprised);
   int surprised_level = 0;
-  eye.ready_for_surprised_eye(path_image_surprised_iris_right);
   for (int i=0; i<50; i++) {
       delay(20);
       surprised_level = i % Eye::max_surprised_level;
       eye.surprised(look_x, look_y, surprised_level);
-      eye.draw_updated_image();
   }
   delay(1000);
 
+  // 眠い
+  Serial.println("Sleepy");
+  eye.load_eye_images(path_image_outline, path_image_iris, path_image_pupil, path_image_reflex, path_image_upperlid);
   int sleepy_level = 0;
-  eye.ready_for_normal_eye(path_image_iris_right, path_image_upperlid_right);
   for (int i=0; i<50; i++) {
       delay(20);
       sleepy_level = i & Eye::max_sleepy_level;
       eye.sleepy(look_x, look_y, sleepy_level);
-      eye.draw_updated_image();
   }
   delay(1000);
 
+  // 怒り
+  Serial.println("Angry");
+  eye.load_eye_images(path_image_outline_angry, path_image_iris_angry,
+          path_image_pupil_angry, path_image_reflex_angry, path_image_upperlid_angry);
   int angry_level = 0;
-  eye.ready_for_angry_eye(path_image_angry_upperlid_right);
   for (int i=0; i<50; i++) {
       delay(20);
       angry_level = i % Eye::max_angry_level;
       eye.angry(look_x, look_y, angry_level);
-      eye.draw_updated_image();
   }
   delay(1000);
 
+  // 哀しい
+  Serial.println("Sad");
+  eye.load_eye_images(path_image_outline_sad, path_image_iris_sad,
+          path_image_pupil_sad, path_image_reflex_sad, path_image_upperlid_sad);
   int sad_level = 0;
-  eye.ready_for_sad_eye(path_image_sad_upperlid_right);
   for (int i=0; i<50; i++) {
       delay(20);
       sad_level = i % Eye::max_sad_level;
-      eye.sad(look_x, look_y, sad_level);
-      eye.draw_updated_image();
+      eye.sad(look_x, look_y);
   }
   delay(1000);
 
+  // 楽しい
+  Serial.println("Happy");
+  eye.load_eye_images(path_image_outline_happy, path_image_iris_happy,
+          path_image_pupil_happy, path_image_reflex_happy, path_image_upperlid_happy);
   int happy_level = 0;
-  eye.ready_for_happy_eye(path_image_happy_upperlid_right);
   for (int i=0; i<50; i++) {
       delay(20);
       happy_level = i % Eye::max_happy_level;
-      eye.happy(look_x, look_y, happy_level);
-      eye.draw_updated_image();
+      eye.happy(look_x, look_y);
   }
 }
